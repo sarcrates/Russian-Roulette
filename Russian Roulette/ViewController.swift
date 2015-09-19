f4d11eea34d84782322a365e3e1086a77d176cb1
Hana Sakamoto <lttlpplkilppl@me.com>
2015-09-18 23:37:25 -0700

    Initial Commit

---
+++ Russian Roulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Russian Roulette
+//
+//  Created by Hana on 9/18/15.
+//  Copyright © 2015 sarcrates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
