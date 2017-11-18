a3e470712de8ad014b18e6d5215c759996c0a07d
Hana Sakamoto <sarcrates@users.noreply.github.com>
2017-11-18 10:11:00 -0700

    updated to iOS 10.3.

--- Russian Roulette/ViewController.swift
+++ Russian Roulette/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var RussianRouletteLabel: UILabel!
+    @IBOutlet weak var ClickTriggerLabel: UILabel!
+    @IBOutlet weak var TriggerButton: UIButton!
+    @IBOutlet weak var DeclineButton: UIButton!
+    
+    //label for when clicking trigger
+    var result = ["click", "click", "click", "click", "click", "bang!!"]
+    var chooseResult = 0
+    
+    //when clicking decline
+    
+    var declining = ["coward", "scardy cat", "go home to mommy"]
+    var chooseDeclining = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +34,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
+    @IBAction func TriggerAction(_ sender: UIButton) {
+        showingResult()
+        FinalResult()
+        
+    }
+    
+    @IBAction func DeclineAction(_ sender: UIButton) {
+        Declining()
+    }
+    
+    func showingResult(){
+      ClickTriggerLabel.text = "\(result[chooseResult])"
+    FinalResult()
+        
+    }
 
+    func FinalResult(){
+        chooseResult = Int(arc4random_uniform(6))
+    }
 
+    func Declining(){
+        ClickTriggerLabel.text = "\(declining[chooseDeclining])"
+        chooseDeclining = Int(arc4random_uniform(3))
+        
+        
+        
+    }
 }
 
